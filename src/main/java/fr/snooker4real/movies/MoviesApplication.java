36c0dd3072abd774f9526b131c0dda911d672778
snooker4real <cindanojonathan@icloud.com>
2023-04-06 18:12:23 +0200

    Initial commit

---
+++ src/main/java/fr/snooker4real/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package fr.snooker4real.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/snooker4real/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.snooker4real.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
